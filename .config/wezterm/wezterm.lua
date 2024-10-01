ec75f62ae1fd79e26ab085bd6324084f5c97da6d
Trey Bastian <2991824+TreyBastian@users.noreply.github.com>
2024-10-01 19:07:45 +0100

    wezterm changes

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -24,6 +24,7 @@ local config = wezterm.config_builder()
 
 config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
 
 config.font = wezterm.font("SauceCodePro Nerd Font", { weight = "Medium" })
 config.font_size = 15
