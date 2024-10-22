6171dd0bfdcfc0bd91519a431c3b8a183506705f
Trey Bastian <2991824+TreyBastian@users.noreply.github.com>
2024-10-22 18:32:25 +0100

    added dadbod-ui for db browsing

---
+++ .config/nvim/lua/trey/lazy/vim-datbod.lua
@@ -0,0 +1,18 @@
+return {
+	"kristijanhusak/vim-dadbod-ui",
+	dependencies = {
+		{ "tpope/vim-dadbod", lazy = true },
+		{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+	},
+	cmd = {
+		"DBUI",
+		"DBUIToggle",
+		"DBUIAddConnection",
+		"DBUIFindBuffer",
+	},
+	init = function()
+		vim.g.db_ui_use_nerd_fonts = 1
+		vim.keymap.set("n", "<leader>db", "<CMD>DBUIToggle<CR>")
+		vim.keymap.set("n", "<leader>dbf", "<CMD>DBUIFindBuffer<CR>")
+	end,
+}
